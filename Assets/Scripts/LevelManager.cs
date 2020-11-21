e44dd72874e0261d6b103c269e251035e443fd68
jeremyBor <jere.bor@free.fr>
2020-11-21 16:21:47 +0100

    Level management

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private string[] _levels;
+
+    private int _curentlevelIndex = 0;
+
+    void Awake()
+    {
+        SceneManager.LoadSceneAsync(_levels[_curentlevelIndex], LoadSceneMode.Additive);
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.UnloadSceneAsync(_levels[_curentlevelIndex]);
+        _curentlevelIndex++;
+        SceneManager.LoadSceneAsync(_levels[_curentlevelIndex], LoadSceneMode.Additive);
+    }
+}
